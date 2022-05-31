0a6eed82367e0565decbe53f83f8162b68dac6db
mazzdriver <oleg@massdriver95.ru>
2022-05-31 21:12:08 +0300

    MPV version: one number in range 1-6

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 from modules import *
+
+
+def get_number(start, stop):
+	number = random.randint(start, stop)
+	return number
+	
+	
+def main():
+	print(get_number(1, 6))
+
+
+if __name__== "__main__":
+    main()
