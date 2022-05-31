172e087ea635037d9e55a977784c17ac16202ce0
mazzdriver <oleg@massdriver95.ru>
2022-05-31 21:37:27 +0300

    Added settings of dices and sides

--- main.py
+++ main.py
@@ -1,13 +1,31 @@
 from modules import *
 
 
+def beginning():
+	print(text.throw_it)
+	
+
 def get_number(start, stop):
 	number = random.randint(start, stop)
 	return number
+
+
+def get_sides():
+	sides = int(input(text.sides))
+	return sides
+
+
+def get_dices():
+	dices = int(input(text.dices))
+	return dices
 	
 	
 def main():
-	print(get_number(1, 6))
+	beginning()
+	amount_of_dices = get_dices()
+	amount_of_sides = get_sides()
+	for _ in range(amount_of_dices):
+		print(get_number(1, amount_of_sides), end=' ')
 
 
 if __name__== "__main__":
--- text.py
+++ text.py
@@ -1,1 +1,3 @@
 throw_it = 'Throw it!'
+sides = 'Enter how much sides should be on a die: '
+dices = 'Enter how much dices should be thrown: '
